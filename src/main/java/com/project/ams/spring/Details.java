f391f31b065e48ab0a6fea715ba7b5fb2fbb09d2
Parzi68 <anamsmit11@gmail.com>
2024-01-29 14:48:08 +0530

    solved database related problem

---
+++ src/main/java/com/project/ams/spring/Details.java
@@ -0,0 +1,121 @@
+package com.project.ams.spring;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "testing2")
+public class Details {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "source_id",columnDefinition = "serial")
+    private Long source_id;
+
+    @Column(name = "com_port")
+    private int com_port;
+
+    @Column(name = "baud_rate")
+    private int baud_rate;
+
+    @Column(name = "data_bits")
+    private int data_bits;
+
+    @Column(name = "stop_bits")
+    private int stop_bits;
+
+    @Column(name = "parity")
+    private boolean parity;
+
+    @Column(name = "polling_interval")
+    private int polling_interval;
+
+    @Column(name = "time_format")
+    private String time_format;
+
+    @Column(name = "report_interval")
+    private int report_interval;
+
+    @Column(name = "set_time_format")
+    private String set_time_format;
+
+
+
+    public Long getSource_id() {
+        return source_id;
+    }
+
+    public void setSource_id(Long source_id) {
+        this.source_id = source_id;
+    }
+
+    public int getCom_port() {
+        return com_port;
+    }
+
+    public void setCom_port(int com_port) {
+        this.com_port = com_port;
+    }
+
+    public int getBaud_rate() {
+        return baud_rate;
+    }
+
+    public void setBaud_rate(int baud_rate) {
+        this.baud_rate = baud_rate;
+    }
+
+    public int getData_bits() {
+        return data_bits;
+    }
+
+    public void setData_bits(int data_bits) {
+        this.data_bits = data_bits;
+    }
+
+    public int getStop_bits() {
+        return stop_bits;
+    }
+
+    public void setStop_bits(int stop_bits) {
+        this.stop_bits = stop_bits;
+    }
+
+    public boolean isParity() {
+        return parity;
+    }
+
+    public void setParity(boolean parity) {
+        this.parity = parity;
+    }
+
+    public int getPolling_interval() {
+        return polling_interval;
+    }
+
+    public void setPolling_interval(int polling_interval) {
+        this.polling_interval = polling_interval;
+    }
+
+    public String getTime_format() {
+        return time_format;
+    }
+
+    public void setTime_format(String time_format) {
+        this.time_format = time_format;
+    }
+
+    public int getReport_interval() {
+        return report_interval;
+    }
+
+    public void setReport_interval(int report_interval) {
+        this.report_interval = report_interval;
+    }
+
+    public String getSet_time_format() {
+        return set_time_format;
+    }
+
+    public void setSet_time_format(String set_time_format) {
+        this.set_time_format = set_time_format;
+    }
+}
---
+++ src/main/java/com/project/ams/spring/UserRepositoryy.java
@@ -0,0 +1,11 @@
+package com.project.ams.spring;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepositoryy extends JpaRepository<Details, Long> {
+    @Query("SELECT MAX(d.source_id) FROM Details d")
+    Long findMaxId();
+}
