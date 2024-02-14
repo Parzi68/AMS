5ffaec8a0f62467cf755e94908886b157b335f7c
Parzi68 <anamsmit11@gmail.com>
2024-02-14 18:39:57 +0530

    Added Components in Tag Mapping page

--- src/main/java/com/project/ams/vaadin/TagMapping.java
+++ src/main/java/com/project/ams/vaadin/TagMapping.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.project.ams.spring.Details;
 import com.project.ams.spring.MapRepository;
 import com.project.ams.spring.MappingData;
 import com.project.ams.views.MainLayout;
@@ -21,12 +22,15 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.router.Route;
 
+import net.wimpi.modbus.net.SerialConnection;
+import net.wimpi.modbus.util.SerialParameters;
+
 @Route(value = "/tagMapping", layout = MainLayout.class)
 public class TagMapping extends VerticalLayout {
-	
+
 	@Autowired
-	MapRepository mapRepository;
-	
+	private final MapRepository mapRepository;
+
 	private final TextField source_id = new TextField("Source Id");
 	private final TextField reg_name = new TextField("Register Name");
 	private final TextField reg_address = new TextField("Register Address");
@@ -37,12 +41,14 @@ public class TagMapping extends VerticalLayout {
 	private final Select<String> point_type = new Select<>();
 	private final Button backbtn = new Button("Back");
 	private final Button submitbtn = new Button("Submit");
+	private final Button connbtn = new Button("Connect");
 	private final Button producebtn = new Button("Kafka Produce");
-	
-	Grid<MappingData> grid = new Grid<>(MappingData.class,false);
-	ListDataProvider<MappingData> dataProvider;
+	private final Button dashboard = new Button("View Dashboard");
+	private final Button stopbtn = new Button("Stop Producing");
 
-	
+	private Grid<MappingData> grid = new Grid<>(MappingData.class);
+	private ListDataProvider<MappingData> dataProvider;
+
 	public TagMapping(MapRepository mapRepository) {
 		this.mapRepository = mapRepository;
 		setSizeFull();
@@ -93,66 +99,66 @@ public class TagMapping extends VerticalLayout {
 		point_type.setItems("01: COIL STATUS", "02: INPUT STATUS", "03: HOLDING REGISTER", "04: INPUT REGISTER");
 		point_type.setErrorMessage("This field is required");
 		point_type.setWidthFull();
-		
-		// Fetch the latest ID from the database and increment it by 1
-				Long nextId = mapRepository.findMaxId();
 
-				// Set the calculated ID as the value of the sourceIdField
-				nextId = (nextId == null) ? 1L : nextId + 1;
-				source_id.setValue(String.valueOf(nextId));
-		
+		// Fetch the latest ID from the database and increment it by 1
+		Long nextId = mapRepository.findMaxId();
+
+		// Set the calculated ID as the value of the sourceIdField
+		nextId = (nextId == null) ? 1L : nextId + 1;
+		source_id.setValue(String.valueOf(nextId));
 
 		backbtn.addClickListener(e -> {
 			UI.getCurrent().navigate(RTUConfig.class);
 		});
 
 		submitbtn.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-//		submitbtn.addClickListener(e -> {
-//			UI.getCurrent().getPage().reload();
-//		});
 		submitbtn.addClickListener(e -> {
 			SaveTags();
-//			Comm();
-			
+		});
+
+		connbtn.addThemeVariants(ButtonVariant.LUMO_PRIMARY,ButtonVariant.LUMO_SUCCESS);
+		connbtn.addClickListener(e -> {
+			Comm();
 		});
 
 		producebtn.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_CONTRAST);
+		
+		dashboard.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+		
+		stopbtn.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE, ButtonVariant.LUMO_ERROR);
 
-		HorizontalLayout buttonLayout = new HorizontalLayout(backbtn, submitbtn, producebtn);
+		HorizontalLayout buttonLayout = new HorizontalLayout(backbtn, submitbtn, connbtn, producebtn, dashboard, stopbtn);
 		VerticalLayout v1 = new VerticalLayout(source_id, reg_name, reg_address, reg_length);
 		VerticalLayout v2 = new VerticalLayout(reg_type, multiplier, element_name, point_type);
 		HorizontalLayout form = new HorizontalLayout(v1, v2);
 		form.setSpacing(true);
-		form.setSizeFull();
-		buttonLayout.setSizeFull();
+		form.setWidthFull();
+		// buttonLayout.setSizeFull();
 		add(form, buttonLayout);
-		
-//		    grid.addColumn(MappingData::getSource_id).setHeader("Source Id");
-//		    grid.addColumn(MappingData::getReg_name).setHeader("Register Name");
-//		    grid.addColumn(MappingData::getReg_address).setHeader("Register Address");
-//		    grid.addColumn(MappingData::getReg_length).setHeader("Register Length");
-//		    grid.addColumn(MappingData::getReg_type).setHeader("Register Data Type");
-//		    grid.addColumn(MappingData::getMultiplier).setHeader("Multiplier");
-//		    grid.addColumn(MappingData::getElement_name).setHeader("Element Name");
-//		    grid.addColumn(MappingData::getPoint_type).setHeader("Modbus Point Type");
-//		    
+		grid.removeAllColumns();
+		grid.addColumn(MappingData::getSource_id).setHeader("Source Id");
+		grid.addColumn(MappingData::getReg_name).setHeader("Register Name");
+		grid.addColumn(MappingData::getReg_address).setHeader("Register Address");
+		grid.addColumn(MappingData::getReg_length).setHeader("Register Length");
+		grid.addColumn(MappingData::getReg_type).setHeader("Register Data Type");
+		grid.addColumn(MappingData::getMultiplier).setHeader("Multiplier");
+		grid.addColumn(MappingData::getElement_name).setHeader("Element");
+		grid.addColumn(MappingData::getPoint_type).setHeader("Modbus Point Type");
+
+		List<MappingData> list = mapRepository.findAll();
+//        System.out.println("Retrieved data from repository: " + list); // Debugging line
+		dataProvider = new ListDataProvider<>(list);
+		grid.setDataProvider(dataProvider);
+		grid.setAllRowsVisible(true);
+
 //		    List<MappingData> list = mapRepository.findAll();
 //			dataProvider = new ListDataProvider<>(list);
 //			grid.setItems(list);
 //			grid.setDataProvider(dataProvider);
-		    //List<MappingData> mappingData = mapRepository.findAll();
-		    //grid.setItems(mapRepository.findAll());
-		update();
-		Grid.Column<MappingData> Source_id = grid.addColumn(MappingData::getSource_id).setHeader("Source id");
-		
-		add(new Hr(), grid);
-	}
+//		    List<MappingData> mappingData = mapRepository.findAll();
+//		    grid.setItems(mapRepository.findAll());
 
-public void update() {
-		List<MappingData> list = mapRepository.findAll();
-		dataProvider = new ListDataProvider<>(list);
-		grid.setItems(list);
-		
+		add(new Hr(), grid);
 	}
 
 //	private void getSavedMappings() {
@@ -169,81 +175,64 @@ public void update() {
 //	    grid.setItems(mapRepository.findAll());
 //	}
 
-		private void SaveTags() {
-			MappingData mappingData = new MappingData();
-			mappingData.setReg_name(reg_name.getValue());
-			mappingData.setReg_address(Integer.parseInt(reg_address.getValue()));
-			mappingData.setReg_length(Integer.parseInt(reg_length.getValue()));
-			mappingData.setReg_type(reg_type.getValue());
-			mappingData.setMultiplier(multiplier.getValue());
-			mappingData.setElement_name(element_name.getValue());
-			mappingData.setPoint_type(point_type.getValue());
-			mapRepository.save(mappingData);
-			Notification.show("Tags Saved!");
+	private void SaveTags() {
+		MappingData mappingData = new MappingData();
+		mappingData.setReg_name(reg_name.getValue());
+		mappingData.setReg_address(Integer.parseInt(reg_address.getValue()));
+		mappingData.setReg_length(Integer.parseInt(reg_length.getValue()));
+		mappingData.setReg_type(reg_type.getValue());
+		mappingData.setMultiplier(multiplier.getValue());
+		mappingData.setElement_name(element_name.getValue());
+		mappingData.setPoint_type(point_type.getValue());
+		mapRepository.save(mappingData);
+		Notification.show("Tags Saved!");
 //			clearForm();
 //			getSavedMappings();
-}
-		/*
-		 * 
-		 * Take the Polling interval value and time format value. in the kafka produce
-		 * method, add the logic for time format for eg. if the time is in ms then
-		 * convert seconds, mins and hrs acc. and multiply the polling value with it.
-		 * add the thread and add the polling value. produce after that interval
-		 * 
-		 * 
-		 * also add the logic to stop the production.
-		 * 
-		 * 
-		 * add the grid when the data is saved0
-		 */
-//		private void clearForm() {
-//			//UI.getCurrent().getPage().executeJs("setTimeout(function() { location.reload(); }, 5000);");
-//			 // Reload only the form by executing JavaScript
-//		    UI.getCurrent().getPage().executeJs("location.reload();");
-//		}
+	}
+	/*
+	 * 
+	 * Take the Polling interval value and time format value. in the kafka produce
+	 * method, add the logic for time format for eg. if the time is in ms then
+	 * convert seconds, mins and hrs acc. and multiply the polling value with it.
+	 * add the thread and add the polling value. produce after that interval
+	 * 
+	 * 
+	 * also add the logic to stop the production.
+	 * 
+	 * 
+	 * add the grid when the data is saved0
+	 */
 
-		// Configure the grid
-//        grid.setColumns("sourceId", "regName", "regAddress", "regLength", "regType",
-//                "multiplier", "elementName", "pointType");
-//        // Remove the line related to "savedDate"
-//        grid.setDataProvider(getSavedMappings());
-//
-//
-//        add(form, buttonLayout);
-//        add(new Hr(), grid);
-//	}
-//
+	private void Comm() {
+		SerialConnection con = null;
+		Details details = new Details();
+		try {
+			SerialParameters params = new SerialParameters();
+			params.setPortName("COM3");
+			params.setBaudRate("9600");
+			params.setDatabits("8");
+			params.setParity("none");
+			params.setStopbits("1"); // only for hubli
+			params.setEncoding("RTU");
+			params.setEcho(false);
+			con = new SerialConnection(params);
 
+			if (!con.isOpen()) {
+				con.open();
+				Notification.show("Modbus RTU Device Connected Successfully").setDuration(3000);
+				// UI.getCurrent().navigate(TagMapping.class);
+				Notification.show("Reading....").setDuration(3000);
+				con.close();
+			} else {
+				Notification.show("Failed to connect to Modbus RTU Device").setDuration(3000);
+				con.close();
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+			Notification.show("An error occurred while trying to connect to Modbus RTU Device").setDuration(3000);
+			con.close();
+		}
 
-
-
-//	private void Comm() {
-//		SerialConnection con = null;
-//		Details details = new Details();
-//		try {
-//			SerialParameters params = new SerialParameters();
-//			params.setPortName(details.getCom_port());
-//			params.setBaudRate(details.getBaud_rate());
-//			params.setDatabits(details.getData_bits());
-//			params.setParity(details.getParity());
-//			params.setStopbits(details.getStop_bits()); // only for hubli
-//			params.setEncoding("RTU");
-//			params.setEcho(false);
-//			con = new SerialConnection(params);
-//
-//			if (!con.isOpen()) {
-//				con.open();
-//				Notification.show("Modbus RTU Device Connected Successfully").setDuration(3000);
-//				//UI.getCurrent().navigate(TagMapping.class);
-//				Notification.show("Reading....").setDuration(3000);
-//			} else {
-//				Notification.show("Failed to connect to Modbus RTU Device").setDuration(3000);
-//			}
-//		} catch (Exception e) {
-//			System.out.println(e);
-//			Notification.show("An error occurred while trying to connect to Modbus RTU Device").setDuration(3000);
-//		}
-//		
-//	}
+	}
 
 }
